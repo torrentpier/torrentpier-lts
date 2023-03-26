fd0a354d20ac0c99e9f4a215fd2642ea7a1c5503
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-03-26 13:16:06 +0700

    Minor fixes

--- library/includes/cron/jobs/sitemap.php
+++ library/includes/cron/jobs/sitemap.php
@@ -7,9 +7,9 @@ require(CLASS_DIR .'sitemap.php');
 $map = new sitemap();
 $map->create();
 
-if (@file_exists(BB_ROOT. "/sitemap/sitemap.xml"))
+if (@file_exists(SITEMAP_DIR. 'sitemap.xml'))
 {
-	$map_link = make_url('/sitemap/sitemap.xml');
+    $map_link = make_url(hide_bb_path(SITEMAP_DIR. 'sitemap.xml'));
 
 	foreach ($bb_cfg['sitemap_sending'] as $source_name => $source_link)
     {
