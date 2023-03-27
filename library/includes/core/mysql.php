47641f59c54ab4c8508b50a1c9870edc5b09e66d
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-03-28 01:08:53 +0700

    Added translations for copy action (#71)

--- library/includes/core/mysql.php
+++ library/includes/core/mysql.php
@@ -898,6 +898,8 @@ class sql_db
 	*/
 	function explain ($mode, $html_table = '', $row = '')
 	{
+        global $lang;
+
 		$query = str_compact($this->cur_query);
 		// remove comments
 		$query = preg_replace('#(\s*)(/\*)(.*)(\*/)(\s*)#', '', $query);
@@ -945,7 +947,7 @@ class sql_db
 				<table width="98%" cellpadding="0" cellspacing="0" class="bodyline row2 bCenter" style="border-bottom: 0px;">
 				<tr>
 					<th style="height: 22px; cursor: pointer;" align="left">&nbsp;'. $dbg['src'] .'&nbsp; ['. sprintf('%.4f', $dbg['time']) .' s]&nbsp; <i>'. $dbg['info'] .'</i></th>
-					<th style="height: 22px; cursor: pointer;" align="right" title="Copy to clipboard" onclick="$(\'#'. $htid .'\').CopyToClipboard();">'. "$this->db_server.$this->selected_db" .' :: Query #'. ($this->num_queries+1) .'&nbsp;</th>
+					<th style="height: 22px; cursor: pointer;" align="right" title="'. $lang['COPY_TO_CLIPBOARD'] .'" onclick="$(\'#'. $htid .'\').CopyToClipboard();">'. "$this->db_server.$this->selected_db" .' :: Query #'. ($this->num_queries+1) .'&nbsp;</th>
 				</tr>
 				<tr><td colspan="2">'. $this->explain_hold .'</td></tr>
 				</table>
