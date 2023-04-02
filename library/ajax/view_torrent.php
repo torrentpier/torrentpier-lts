e74bf1182599671fef1c5605910ac4fe8028f384
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-04-02 13:40:44 +0700

    Torrent file content sort fix

--- library/ajax/view_torrent.php
+++ library/ajax/view_torrent.php
@@ -133,7 +133,7 @@ class torrent
 							$cur_files_ary[] = $this->build_file_item($name, $length);
 						}
 					}
-					@natsort($cur_files_ary);
+					asort($cur_files_ary);
 				}
 				else
 				{
