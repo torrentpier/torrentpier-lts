92f99ff66b96ff8adeaf3b572797b1c589004c47
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-04-09 17:05:48 +0700

    Minor fixes (#148)

--- library/includes/core/mysql.php
+++ library/includes/core/mysql.php
@@ -95,7 +95,7 @@ class sql_db
 		{
 			$server = (DBG_USER) ? $this->cfg['dbhost'] : '';
 			header("HTTP/1.0 503 Service Unavailable");
-			bb_log(' ', "db_err/connect_failed_{$this->cfg['dbhost']}");
+			bb_log("Could not connect to mysql server $server", ("db_err/connect_failed_{$this->cfg['dbhost']}_" . time()));
 			die("Could not connect to mysql server $server");
 		}
 
