33fba867a61885478ec45bc224302fffc1b49374
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-06-30 18:45:19 +0700

    fix

--- library/includes/sessions.php
+++ library/includes/sessions.php
@@ -334,11 +334,11 @@ class user_common
 	*/
 	function session_end ($update_lastvisit = false, $set_cookie = true)
 	{
+		cache_rm_userdata($this->data);
 		DB()->query("
 			DELETE FROM ". BB_SESSIONS ."
 			WHERE session_id = '{$this->data['session_id']}'
 		");
-		cache_rm_userdata($this->data);
 
 		if (!IS_GUEST)
 		{
@@ -359,11 +359,11 @@ class user_common
 			{
 				$this->create_autologin_id($this->data, false);
 
+				cache_rm_user_sessions($this->data['user_id']);
 				DB()->query("
 					DELETE FROM ". BB_SESSIONS ."
 					WHERE session_user_id = '{$this->data['user_id']}'
 				");
-				cache_rm_user_sessions($this->data['user_id']);
 			}
 		}
 
