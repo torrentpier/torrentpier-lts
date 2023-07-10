611c17e34d6bcd74e8504b155cfa8603bc081bd6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-07-10 11:44:53 +0700

    Fix RFC 1918 RegExp

--- bt/announce.php
+++ bt/announce.php
@@ -118,7 +118,7 @@ if (!$bb_cfg['ignore_reported_ip'] && isset($_GET['ip']) && $ip !== $_GET['ip'])
 		{
 			if ($x_ip === $_GET['ip'])
 			{
-				if (!$bb_cfg['allow_internal_ip'] && preg_match("#^(10|172\.16|192\.168)\.#", $x_ip))
+				if (!$bb_cfg['allow_internal_ip'] && preg_match("#(127\.([0-9]{1,3}\.){2}[0-9]{1,3}|10\.([0-9]{1,3}\.){2}[0-9]{1,3}|172\.[123][0-9]\.[0-9]{1,3}\.[0-9]{1,3}|192\.168\.[0-9]{1,3}\.[0-9]{1,3})#", $x_ip))
 				{
 					break;
 				}
