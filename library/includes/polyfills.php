a3a8d242b51c6405fc85f545609ec0dab089463f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-08-07 13:29:06 +0700

    Update polyfills.php

--- library/includes/polyfills.php
+++ library/includes/polyfills.php
@@ -35,18 +35,21 @@ if (!function_exists('class_uses')) {
 if (50509 === PHP_VERSION_ID && 4 === PHP_INT_SIZE) {
     // Missing functions in PHP 5.5.9 - affects 32 bit builds of Ubuntu 14.04LTS
     // See https://bugs.launchpad.net/ubuntu/+source/php5/+bug/1315888
+
     if (!function_exists('gzopen') && function_exists('gzopen64')) {
         function gzopen($filename, $mode, $use_include_path = 0)
         {
             return gzopen64($filename, $mode, $use_include_path);
         }
     }
+
     if (!function_exists('gzseek') && function_exists('gzseek64')) {
         function gzseek($fp, $offset, $whence = SEEK_SET)
         {
             return gzseek64($fp, $offset, $whence);
         }
     }
+
     if (!function_exists('gztell') && function_exists('gztell64')) {
         function gztell($fp)
         {
