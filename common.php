3f53d327e99aa51a8b2ffed8f89c9f5636f36f0b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-08-20 12:16:41 +0700

    Revert "Updated"
    This reverts commit f029df8445e8822b23e773d6ff33edd1a100d348.

--- common.php
+++ common.php
@@ -83,19 +83,6 @@ function sqlite3_escape_string ($str)
 }
 
 /**
- * html_entity_decode replacement (from php manual)
- */
-if (!function_exists('html_entity_decode'))
-{
-	function html_entity_decode($given_html, $quote_style = ENT_QUOTES)
-	{
-		$trans_table = array_flip(get_html_translation_table(HTML_SPECIALCHARS, $quote_style));
-		$trans_table['&#39;'] = "'";
-		return (strtr($given_html, $trans_table));
-	}
-}
-
-/**
  * Database
  */
 // Core DB class
