70c884db8cc2ce1b627ec73559b8d2c3eb5e2ef6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-09-19 16:32:27 +0700

    Updated

--- library/includes/cron/jobs/clean_log.php
+++ library/includes/cron/jobs/clean_log.php
@@ -4,7 +4,7 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 $log_days_keep = (int) $bb_cfg['log_days_keep'];
 
-if ($log_days_keep)
+if ($log_days_keep != 0)
 {
 	DB()->query("
 		DELETE FROM ". BB_LOG ."
--- library/includes/cron/jobs/clean_pm.php
+++ library/includes/cron/jobs/clean_pm.php
@@ -4,7 +4,7 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 $pm_days_keep = (int) $bb_cfg['pm_days_keep'];
 
-if ($pm_days_keep)
+if ($pm_days_keep != 0)
 {
 	DB()->query("
 		DELETE FROM ". BB_PRIVMSGS ."
