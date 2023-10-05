05d1e0fc2ec6cba4e09535d89df94096457303b8
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-05 11:34:00 +0700

    Update group.php

--- group.php
+++ group.php
@@ -19,7 +19,7 @@ function generate_user_info(&$row, $date_format, $group_mod, &$from, &$posts, &$
 	$from      = (!empty($row['user_from'])) ? $row['user_from'] : $lang['NOSELECT'];
 	$joined    = bb_date($row['user_regdate'], $date_format);
 	$user_time = (!empty($row['user_time'])) ? sprintf('%s <span class="posted_since">(%s)</span>', bb_date($row['user_time'], $date_format), delta_time($row['user_time'])) : $lang['NONE'];
-	$posts     = ($row['user_posts']) ? '<a href="search.php?search_author=1&amp;uid='.$row['user_id'].'" target="_blank">'. $row['user_posts'] .'</a>' : 0;
+	$posts     = '<a href="search.php?search_author=1&amp;uid='.$row['user_id'].'" target="_blank">'. $row['user_posts'] .'</a>';
 	$pm        = ($bb_cfg['text_buttons']) ? '<a class="txtb" href="'. (PM_URL . "?mode=post&amp;". POST_USERS_URL ."=".$row['user_id']) .'">'. $lang['SEND_PM_TXTB'] .'</a>' : '<a href="' . (PM_URL . "?mode=post&amp;". POST_USERS_URL ."=".$row['user_id']) .'"><img src="' . $images['icon_pm'] . '" alt="' . $lang['SEND_PRIVATE_MESSAGE'] . '" title="' . $lang['SEND_PRIVATE_MESSAGE'] . '" border="0" /></a>';
 	$avatar    = get_avatar($row['user_id'], $row['avatar_ext_id'], !bf($row['user_opt'], 'user_opt', 'dis_avatar'), '', 50, 50);
 
