71df8f8f989b553b927383425fbcc50f77ce034a
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-06 14:49:52 +0700

    Minor improvements

--- admin/admin_bt_tracker_cfg.php
+++ admin/admin_bt_tracker_cfg.php
@@ -7,7 +7,7 @@ if (!empty($setmodules))
 }
 require('./pagestart.php');
 
-if (!IS_SUPER_ADMIN) bb_die($lang['NOT_ADMIN']);
+if (!IS_SUPER_ADMIN) bb_die($lang['ONLY_FOR_SUPER_ADMIN']);
 
 require(INC_DIR .'functions_admin_torrent.php');
 
--- library/ajax/topic_tpl.php
+++ library/ajax/topic_tpl.php
@@ -4,7 +4,7 @@ if (!defined('IN_AJAX')) die(basename(__FILE__));
 
 global $userdata, $lang;
 
-if (!IS_SUPER_ADMIN) $this->ajax_die($lang['NOT_AUTHORISED']);
+if (!IS_SUPER_ADMIN) $this->ajax_die($lang['ONLY_FOR_SUPER_ADMIN']);
 
 array_deep($this->request, 'trim');
 
