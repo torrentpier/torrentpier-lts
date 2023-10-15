9bbe0a6bebd72e734b2e7d79ae1f6f988efa6412
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-16 00:17:45 +0700

    Minor improvements (#322)
    * Minor improvements
    
    * Update CHANGELOG.md

--- library/includes/ucp/register.php
+++ library/includes/ucp/register.php
@@ -59,7 +59,7 @@ switch ($mode)
 			// Ограничение по времени
 			else if ($bb_cfg['new_user_reg_restricted'])
 			{
-				if (in_array(date('G'), array(0,/*1,2,3,4,5,6,7,8,11,12,13,14,15,16,*/17,18,19,20,21,22,23)))
+				if (in_array(bb_date(TIMENOW, 'H', false), $bb_cfg['new_user_reg_restricted']))
 				{
 					bb_die($lang['REGISTERED_IN_TIME']);
 				}
