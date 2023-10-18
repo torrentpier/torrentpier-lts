e40cf09b93e3bc1049a5afca7562384c08705c78
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-18 19:03:14 +0700

    Update view_torrent.php

--- library/ajax/view_torrent.php
+++ library/ajax/view_torrent.php
@@ -17,7 +17,7 @@ $torrent = DB()->fetch_row("SELECT at.attach_id, at.physical_filename FROM ". BB
 if (!$torrent) $this->ajax_die($lang['EMPTY_ATTACH_ID']);
 $filename = get_attachments_dir() .'/'. $torrent['physical_filename'];
 
-if (!@file_exists($filename) || (false === ($file_contents = @file_get_contents($filename))))
+if ((($file_contents = @file_get_contents($filename)) === false) || !file_exists($filename))
 {
 	if (IS_AM)
 	{
