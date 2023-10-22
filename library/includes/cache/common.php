3670bf7c1d260d90d98bc0ee19d7470ee0d57caf
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-22 17:34:55 +0700

    Updated

--- library/includes/cache/common.php
+++ library/includes/cache/common.php
@@ -77,7 +77,7 @@ class cache_common
 		if (!SQL_PREPEND_SRC_COMM) return 'src disabled';
 		foreach (debug_backtrace() as $trace)
 		{
-			if (empty($trace['file']) || empty($trace['line'])) break;
+			if (empty($trace['file']) || empty($trace['line'])) continue;
 			if ($trace['file'] !== __FILE__)
 			{
 				switch ($mode)
--- library/includes/datastore/common.php
+++ library/includes/datastore/common.php
@@ -168,7 +168,7 @@ class datastore_common
 		if (!SQL_PREPEND_SRC_COMM) return 'src disabled';
 		foreach (debug_backtrace() as $trace)
 		{
-			if (empty($trace['file']) || empty($trace['line'])) break;
+			if (empty($trace['file']) || empty($trace['line'])) continue;
 			if ($trace['file'] !== __FILE__)
 			{
 				switch ($mode)
