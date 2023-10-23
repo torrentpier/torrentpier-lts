521cab7243e689732233857faefc7968582029dc
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-23 09:20:10 +0700

    Update functions_dev.php

--- library/includes/functions_dev.php
+++ library/includes/functions_dev.php
@@ -47,7 +47,7 @@ function get_sql_log_html ($db_obj, $log_name)
 	{
 		$id   = "sql_{$i}_". mt_rand();
 		$sql  = short_query($dbg['sql'], true);
-		$time = SQL_CALC_QUERY_TIME ? sprintf('%.4f', $dbg['time']) : '';
+		$time = SQL_CALC_QUERY_TIME ? (sprintf('%.4f', $dbg['time']) . 's') : '';
 		$perc = SQL_CALC_QUERY_TIME ? @sprintf('[%d%%]', round($dbg['time']*100/$db_obj->sql_timetotal)) : '';
 		$info = !empty($dbg['info']) ? $dbg['info'] .' ['. $dbg['src'] .']' : $dbg['src'];
 
