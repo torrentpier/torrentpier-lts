7924ef19556d96b7ba494284a35689c9cb5c838b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-23 09:01:20 +0700

    Update mysql.php

--- library/includes/core/mysql.php
+++ library/includes/core/mysql.php
@@ -952,10 +952,11 @@ class sql_db
 				$htid = 'expl-'. intval($this->link) .'-'. $id;
 				$dbg  = $this->dbg[$id];
 
+				$query_time = SQL_CALC_QUERY_TIME ? ('&nbsp;[' . sprintf('%.4f', $dbg['time']) . 's]&nbsp;') : '&nbsp;';
 				$this->explain_out .= '
 					<table width="98%" cellpadding="0" cellspacing="0" class="bodyline row2 bCenter" style="border-bottom: 0px;">
 						<tr>
-							<th style="height: 22px; cursor: pointer;" align="left">&nbsp;'. $dbg['src'] .'&nbsp; ['. sprintf('%.4f', $dbg['time']) .' s]&nbsp; <i>'. $dbg['info'] .'</i></th>
+							<th style="height: 22px; cursor: pointer;" align="left">&nbsp;'. $dbg['src'] . $query_time .'<i>'. $dbg['info'] .'</i></th>
 							<th style="height: 22px; cursor: pointer;" align="right" title="Copy to clipboard" onclick="$(\'#'. $htid .'\').CopyToClipboard();">'. "[$this->engine] $this->db_server.$this->selected_db" .' :: Query #'. ($this->num_queries+1) .'&nbsp;</th>
 						</tr>
 						<tr><td colspan="2">'. $this->explain_hold .'</td></tr>
