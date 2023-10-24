2554020745c838b2996731bbe14d2b21501b2258
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-24 16:24:34 +0700

    Update torrent_announce_urls.php

--- library/includes/torrent_announce_urls.php
+++ library/includes/torrent_announce_urls.php
@@ -4,8 +4,10 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 $announce_urls = array();
 
-// Здесь вы можете указать дополнительные разрешенные URL-адреса анонсеров которые будут добавляться автоматически в ваши раздачи
-// Например, если вы хотите добавить http://demo.torrentpier.com
+// Здесь вы можете указать дополнительные разрешенные URL-адреса анонсеров
+// Например, если вы хотите добавить анонсер http://demo.torrentpier.com
 // добавьте эту строку: $announce_urls[] = 'http://demo.torrentpier.com/bt/announce.php';
+//
+// Важно, добавляйте URL-адреса без GET параметров в конце
 
 // $announce_urls[] = 'http://demo.torrentpier.com/bt/announce.php';
