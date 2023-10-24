13d1474209148b4db1cc91bbec601f797caafa58
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-24 22:29:08 +0700

    Update torrent_announce_urls_additional.php

--- library/includes/torrent_announce_urls_additional.php
+++ library/includes/torrent_announce_urls_additional.php
@@ -4,9 +4,11 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 $additional_announce_urls = array();
 
-// Здесь вы можете указать дополнительные URL-адреса анонсеров, которые будут автоматически вставляться в ваши раздачи
+// Здесь вы можете указать дополнительные URL-адреса анонсеров, которые будут добавляться к вашим раздачам
+//
+// Примечание:
+// - Анонсеры с GET параметрами (например passkey или иной аутентификатор доступа) лучше не указывать
 //
 // Примеры:
-// $additional_announce_urls[] = 'http://demo.torrentpier.com/bt/announce.php';
 // $additional_announce_urls[] = 'http://tracker.openbittorrent.com:80/announce';
 // $additional_announce_urls[] = 'udp://tracker.openbittorrent.com:6969/announce';
