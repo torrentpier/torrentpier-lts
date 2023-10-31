822c5af56e3b0f8edeed4e5670e2e099d791b042
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-31 16:18:36 +0700

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -142,7 +142,7 @@ class ajax_common
 		// Exit if board is disabled via ON/OFF trigger or by admin
 		if ($bb_cfg['board_disable'] || file_exists(BB_DISABLED))
 		{
-			if ($action_params[AJAX_ALWAYS_ACTIVE] !== true)
+			if (!isset($action_params[AJAX_ALWAYS_ACTIVE]) || $action_params[AJAX_ALWAYS_ACTIVE] === false)
 			{
 				if ($bb_cfg['board_disable'])
 				{
