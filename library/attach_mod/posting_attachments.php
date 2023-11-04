dc6ee05fb9f790b4b8d8e71748bc900219a4f964
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-11-05 01:46:44 +0700

    Update posting_attachments.php

--- library/attach_mod/posting_attachments.php
+++ library/attach_mod/posting_attachments.php
@@ -929,7 +929,7 @@ class attach_parent
 
 			//bt
 			// Block uploading more than one torrent file
-			$uploaded_torrents = isset(array_count_values($this->attachment_extension_list)[TORRENT_EXT]) ? array_count_values($this->attachment_extension_list)[TORRENT_EXT] : 0;
+			$uploaded_torrents = isset(array_count_values($this->attachment_extension_list)[TORRENT_EXT]) ? (int) array_count_values($this->attachment_extension_list)[TORRENT_EXT] : 0;
 			if (!$error && $this->extension === TORRENT_EXT && ($uploaded_torrents >= 1))
 			{
 				$error = TRUE;
