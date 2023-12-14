ea22a171d5946671398493a548ce33eecc27317e
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-12-14 18:05:01 +0700

    Update common.php

--- common.php
+++ common.php
@@ -82,7 +82,7 @@ define('FULL_URL', $server_protocol . $bb_cfg['server_name'] . $server_port . $b
 unset($server_protocol, $server_port);
 
 // Debug options
-define('DBG_USER', !empty($_COOKIE[COOKIE_DBG]));
+define('DBG_USER', !empty($_COOKIE[COOKIE_DBG]) && $_COOKIE[COOKIE_DBG] == 1);
 
 // Board/Tracker shared constants and functions
 define('BB_BT_TORRENTS',     'bb_bt_torrents');
