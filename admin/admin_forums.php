9fe54644f84417a9ca7e01e8d6f2e8d3c0395d4b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-07 15:40:11 +0700

    Updated

--- admin/admin_forums.php
+++ admin/admin_forums.php
@@ -812,7 +812,7 @@ if (!$mode || $show_main_page)
 						'FORUM_PARENT'      => $forum_rows[$j]['forum_parent'],
 						'SF_PAD'            => ($forum_rows[$j]['forum_parent']) ? ' style="padding-left: 20px;" ' : '',
 						'FORUM_NAME_CLASS'  => ($forum_rows[$j]['forum_parent']) ? 'genmed' : 'gen',
-						'ADD_SUB_HREF'      => "admin_forums.php?mode=addforum&amp;forum_parent={$forum_rows[$j]['forum_id']}",
+						'ADD_SUB_HREF'      => (!$forum_rows[$j]['forum_parent']) ? "admin_forums.php?mode=addforum&amp;forum_parent={$forum_rows[$j]['forum_id']}" : '',
 						'U_VIEWFORUM'       => BB_ROOT . FORUM_URL . $forum_id,
 						'U_FORUM_EDIT'      => "admin_forums.php?mode=editforum&amp;f=$forum_id",
 						'U_FORUM_PERM'      => "admin_forumauth.php?f=$forum_id",
