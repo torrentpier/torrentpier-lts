60046e58a23a5ad9ae7227a7ae9f0ab497ff892f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-11 15:16:59 +0700

    Update admin_phpinfo.php

--- admin/admin_phpinfo.php
+++ admin/admin_phpinfo.php
@@ -2,7 +2,7 @@
 
 if (!empty($setmodules))
 {
-	$module['GENERAL']['PHP_INFO'] = basename(__FILE__);
+	if (IS_SUPER_ADMIN) $module['GENERAL']['PHP_INFO'] = basename(__FILE__);
 	return;
 }
 require('./pagestart.php');
