38ac6b64472b4194444f1480cca7fbee9ffb587f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-18 15:10:10 +0700

    Update init_tr.php

--- bt/includes/init_tr.php
+++ bt/includes/init_tr.php
@@ -54,7 +54,7 @@ function error_exit ($msg = '')
 
 	silent_exit();
 
-	echo bencode(array('failure reason' => str_compact($msg)));
+	// echo bencode(array('failure reason' => str_compact($msg)));
 
 	tracker_exit();
 }
