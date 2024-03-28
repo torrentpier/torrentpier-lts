227a9768a39a944144c7cacb209ec0912cfc9240
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-28 14:40:13 +0700

    Update admin_terms.php

--- admin/admin_terms.php
+++ admin/admin_terms.php
@@ -12,7 +12,7 @@ require(INC_DIR .'bbcode.php');
 if (isset($_POST['post']) && $bb_cfg['terms'] != $_POST['message'])
 {
 	bb_update_config(array('terms' => $_POST['message']));
-	bb_die($lang['CONFIG_UPDATED']);
+	bb_die($lang['SAVED'] . '<br /><br /><a href="admin_terms.php">' . $lang['GO_BACK'] . '</a><br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="index.php?pane=right">', '</a>'));
 }
 
 $template->assign_vars(array(
