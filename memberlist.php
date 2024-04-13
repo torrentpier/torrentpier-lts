f8d485397a867e8c05d5da240be886e445431119
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-13 13:35:03 +0700

    Update memberlist.php

--- memberlist.php
+++ memberlist.php
@@ -6,7 +6,7 @@ define('BB_ROOT', './');
 require(BB_ROOT .'common.php');
 
 $show_avatars_memberlist = false;
-$page_cfg['use_tablesorter'] = true;
+$page_cfg['use_tablesorter'] = false; // Отключен поскольку на странице уже есть сортировка
 
 $user->session_start(array('req_login' => true));
 
