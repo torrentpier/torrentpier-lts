b9b532cbfafff9a7d0159f7c4b038e2dbdc56797
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-13 12:41:51 +0700

    Revert "Updated"
    This reverts commit 81975d9fc635e0d714db1022896640a0dfd934b1.

--- library/config.php
+++ library/config.php
@@ -588,8 +588,6 @@ $bb_cfg['atom'] = array(
 	// Примечание: Без слэша в конце
 	'path' => INT_DATA_DIR .'atom',
 	'url'  => './internal_data/atom',
-	'direct_down' => true, // Разрешить прямую загрузку торрентов из atom ленты
-	'direct_view' => true, // Разрешить прямой просмотр текстов сообщений из atom ленты
 );
 
 // Nofollow
