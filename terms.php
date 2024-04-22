91a512ce6d153b1e9c194f04761393d004739f80
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-22 15:51:07 +0700

    Update terms.php

--- terms.php
+++ terms.php
@@ -11,7 +11,7 @@ $user->session_start();
 if (!$bb_cfg['terms'] && !IS_ADMIN) redirect('index.php');
 
 $template->assign_vars(array(
-	'TERMS_EDIT' => bbcode2html(sprintf($lang['TERMS_EMPTY_TEXT'], $domain_name)),
+	'TERMS_EDIT' => bbcode2html(sprintf($lang['TERMS_EMPTY_TEXT'], make_url('admin/admin_terms.php'))),
 	'TERMS_HTML' => bbcode2html($bb_cfg['terms']),
 ));
 
