f5e456c183091539bcf531bd0117aaf98409b7ca
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-12-22 12:16:30 +0700

    Update main.php

--- library/language/uk/main.php
+++ library/language/uk/main.php
@@ -52,6 +52,7 @@ $lang['DISABLED'] = 'Вимкнено';
 $lang['ERROR'] = 'Помилка';
 $lang['SELECT_ACTION'] = 'Виберіть дію';
 $lang['CLEAR'] = 'Очистити';
+$lang['MOVE_TO_TOP'] = 'На початок';
 
 $lang['NEXT_PAGE'] = 'Наступ.';
 $lang['PREVIOUS_PAGE'] = 'Попер.';
