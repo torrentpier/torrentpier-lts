e13da60fac862050a26be16824dc1b5975ee1bca
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-21 16:24:42 +0300

    Update yandex_captcha.php

--- library/includes/captcha/yandex_captcha.php
+++ library/includes/captcha/yandex_captcha.php
@@ -12,7 +12,7 @@ function yandex_captcha_check($settings)
 	$ch = curl_init("https://smartcaptcha.yandexcloud.net/validate");
 	$args = [
 		'secret' => $settings['server_key'],
-		'token' => htmlCHR($_POST['smart-token']),
+		'token' => $_POST['smart-token'],
 		'ip' => $_SERVER["REMOTE_ADDR"],
 	];
 
