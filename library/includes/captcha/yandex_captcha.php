7e666b8c8c4b5cf81825ce035ed856cae1eda2f7
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-21 15:35:51 +0300

    Create yandex_captcha.php

---
+++ library/includes/captcha/yandex_captcha.php
@@ -0,0 +1,34 @@
+<?php
+
+function yandex_captcha_get($settings)
+{
+	return "
+		<script src='https://smartcaptcha.yandexcloud.net/captcha.js' defer></script>
+		<div id='captcha-container' class='smart-captcha' data-sitekey='{$settings['client_key']}' data-hl='ru'></div>";
+}
+
+function yandex_captcha_check($settings)
+{
+	$ch = curl_init("https://smartcaptcha.yandexcloud.net/validate");
+	$args = [
+		'secret' => $serverKey,
+		'token' => htmlCHR($_POST['smart-token']),
+		'ip' => $_SERVER["REMOTE_ADDR"],
+	];
+
+	curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$server_output = curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+
+	if ($httpcode !== 200) {
+		return true;
+	}
+
+	$resp = json_decode($server_output);
+	return ($resp->status === 'ok');
+}
