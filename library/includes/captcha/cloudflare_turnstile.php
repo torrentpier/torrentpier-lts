dc5340978b2d953429f2734631eb06f66d0b1c5c
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-21 16:09:29 +0300

    Update cloudflare_turnstile.php

--- library/includes/captcha/cloudflare_turnstile.php
+++ library/includes/captcha/cloudflare_turnstile.php
@@ -9,6 +9,22 @@ function cloudflare_turnstile_get($settings)
 
 function cloudflare_turnstile_check($settings)
 {
+	$turnstile_response = $_POST['cf-turnstile-response'];
+	$url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+	$post_fields = "secret={$settings['secret_key']}&response=$turnstile_response";
 
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	$response_data = json_decode($response);
+	if ($response_data->success == 1) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
