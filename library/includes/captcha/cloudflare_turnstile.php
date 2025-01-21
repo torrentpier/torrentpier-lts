9acbba5df4813707081867a80f4a5a324bad4439
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-21 16:07:08 +0300

    Update cloudflare_turnstile.php

--- library/includes/captcha/cloudflare_turnstile.php
+++ library/includes/captcha/cloudflare_turnstile.php
@@ -1,13 +1,13 @@
 <?php
 
-function recaptcha_v3_get($settings)
+function cloudflare_turnstile_get($settings)
 {
 	return "
 		<script src='https://challenges.cloudflare.com/turnstile/v0/api.js' async defer></script>
 		<div class='cf-turnstile' data-sitekey='{$settings['site_key']}' data-theme='light'></div>";
 }
 
-function recaptcha_v3_check($settings)
+function cloudflare_turnstile_check($settings)
 {
 
 }
