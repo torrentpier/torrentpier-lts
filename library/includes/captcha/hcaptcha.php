0d72d883e23ef1a8fff78e06831a1b152c2f463b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-21 16:26:32 +0300

    Update hcaptcha.php

--- library/includes/captcha/hcaptcha.php
+++ library/includes/captcha/hcaptcha.php
@@ -15,7 +15,7 @@ function hcaptcha_check($settings)
 	);
 
 	$verify = curl_init();
-	curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+	curl_setopt($verify, CURLOPT_URL, 'https://hcaptcha.com/siteverify');
 	curl_setopt($verify, CURLOPT_POST, true);
 	curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
 	curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
